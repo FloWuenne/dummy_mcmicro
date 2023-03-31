c63363f87d2e0db827254a476f4bd7d9d439706a
FloWuenne <flowuenne@gmail.com>
2023-03-31 15:19:49 +0200

    Second commit

--- work/f6/b1c97691d55880ac9ca908ed2edeb0/.command.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -ue
-cellpose     --image_path image.backsub.ome.tif     --save_tif     --verbose     --pretrained_model nuclei --diameter 9 --channel_axis 0 --no_npy
-
-cat <<-END_VERSIONS > versions.yml
-"CELLPOSE":
-    cellpose: 2.1.1
-END_VERSIONS
